d9e922ceddbea8f7b9e84cf8d769142a90d78e14
YALLICO OWNER <yallico2024@gmail.com>
2026-08-30 13:55:27 -0500

    Actualizar group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -3,24 +3,24 @@ let handler = async (m, { conn }) => {
         await m.react('🔗')
         let link = await conn.groupInviteCode(m.chat)
 
-        let texto = `🐱 𓆩 𝗟𝗜𝗡𝗞 𝗗𝗘𝗟 𝗚𝗥𝗨𝗣𝗢 𓆪 🐱
+        let texto = `🎮 𓆩 𝗟𝗜𝗡𝗞 𝗗𝗘𝗟 𝗟𝗢𝗕𝗕𝗬 𓆪 🤖
 
-.⃟𖥔 ݁. 𖦹˙— \`\`INVITACION\`\` —˙𖦹.🍕꒷
+.⃟𖥔 ݁. 𖦹˙— \`\`𝗜𝗡𝗩𝗜𝗧𝗔𝗖𝗜𝗢𝗡\`\` —˙𖦹.🕹️꒷
 
-──🍃 *𝗟𝗜𝗡𝗞* ╏ 💚
+──🎮 *𝗟𝗜𝗡𝗞* ╏ 💚
 https://chat.whatsapp.com/${link}
 
-──🍃 *𝗡𝗢𝗧𝗔* ╏ 🌿
-🍕 *Solo admins pueden resetear el link*
-🍕 *No lo compartas con desconocidos*
+──🤖 *𝗡𝗢𝗧𝗔* ╏ 🕹️
+🎮 *Solo admins pueden resetear el link*
+🎮 *No lo compartas con desconocidos*
 
 ━━━━━━━━━━━
-*Powered by*: ***Garfield Bot Oficial*** 🍕`
+*Powered by*: ***Ricky Bot Oficial*** 🎮`
 
         await conn.reply(m.chat, texto, m)
     } catch (e) {
         await m.react('❌')
-        m.reply(`❌ *Error:* No pude obtener el link. ¿Soy admin?`)
+        m.reply(`🎮 ❌ *Error:* No pude obtener el link. ¿Soy admin? 🤖`)
     }
 }
 
