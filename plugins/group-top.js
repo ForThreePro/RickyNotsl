c64560df21b5d6694a1a5166c34829500dbc3462
YALLICO OWNER <yallico2024@gmail.com>
2026-08-30 13:59:52 -0500

    Actualizar group-top.js

--- plugins/group-top.js
+++ plugins/group-top.js
@@ -4,15 +4,15 @@ import path from 'path'
 let user = a => '@' + a.split('@')[0]
 
 function handler(m, { groupMetadata, command, conn, text }) {
-    if (!groupMetadata) return m.reply('🐱 *Este comando solo funciona en grupos*')
-    if (!text) return m.reply(`🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱
+    if (!groupMetadata) return m.reply('🎮 *Este comando solo funciona en lobbys*')
+    if (!text) return m.reply(`🎮 𓆩 ***𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🤖
 
 *Ejemplo de uso:*
 .top *Mejores en PVP*
 .top *Más activos*`)
 
     let ps = groupMetadata.participants.map(v => v.id)
-    if (ps.length < 10) return m.reply('🍕 *Se necesitan mínimo 10 miembros en el grupo*')
+    if (ps.length < 10) return m.reply('🕹️ *Se necesitan mínimo 10 players en el lobby*')
 
     let a = ps.getRandom()
     let b = ps.getRandom()
@@ -26,16 +26,16 @@ function handler(m, { groupMetadata, command, conn, text }) {
     let j = ps.getRandom()
     let k = Math.floor(Math.random() * 70)
 
-    let emojis = ['🐱','🍕','💤','😼','🙄','😂','👀','🔥','🤑','💩','🥱','😎','😅','👇🏻','😔','🌚','🗿','✨','❤️']
+    let emojis = ['🎮','🤖','🕹️','⚡','🔥','👑','💥','🏆','🎯','🚀','💯','🌟','😎','👇🏻','✨','💢','🗿','❤️','⚔️']
     let x = pickRandom(emojis)
 
     let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
 
-    let top = `🐱 𓆩 𝗧𝗢𝗣 𝟭𝟬 𓆪 🐱
+    let top = `🎮 𓆩 𝗧𝗢𝗣 𝟭𝟬 𓆪 🤖
 
-.⃟𖥔 ݁. 𖦹˙— \`\` ${text.toUpperCase()} \`\` —˙𖦹.🍕꒷
+.⃟𖥔 ݁. 𖦹˙— \`\` ${text.toUpperCase()} \`\` —˙𖦹.🕹️꒷
 
- ⤷ ┇ *RANKING* ：✿ 。
+ ⤷ ┇ *𝗥𝗔𝗡𝗞𝗜𝗡𝗚* ：✿ 。
 
 ${x} *1.* ${user(a)}
 ${x} *2.* ${user(b)}
@@ -49,7 +49,7 @@ ${x} *9.* ${user(i)}
 ${x} *10.* ${user(j)}
 
 ━━━━━━━━━━━
-*Powered by*: ***Garfield Bot Oficial*** 🍕`
+*Powered by*: ***Ricky Bot Oficial*** 🎮`
 
     m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
 }
