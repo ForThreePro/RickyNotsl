9b16e04ba6728bce872b8f511cdb318520e51f07
YALLICO OWNER <yallico2024@gmail.com>
2026-08-30 13:54:13 -0500

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
     let mentionedJid = m.mentionedJid && m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : null
 
-    if (!mentionedJid) return conn.reply(m.chat, `🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱
+    if (!mentionedJid) return conn.reply(m.chat, `🎮 𓆩 ***𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🤖
 
 *Uso:*
-.${command} @user → Para expulsar
-.${command} → Responde al mensaje del user
+.${command} @user → Para expulsar del lobby
+.${command} → Responde al mensaje del player
 
-> *Solo admins*`, m)
+> *Solo admins* 🕹️`, m)
 
     try {
         let groupMetadata = await conn.groupMetadata(m.chat)
@@ -17,26 +17,26 @@ let handler = async (m, { conn, participants, usedPrefix, command }) => {
         let user = participants.find(p => p.id === mentionedJid)
         let isAdmin = user?.admin
 
-        if (mentionedJid === conn.user.jid) return conn.reply(m.chat, `🐱 *No puedo eliminarme a mí mismo.*`, m)
-        if (mentionedJid === ownerGroup) return conn.reply(m.chat, `🍕 *No puedo eliminar al propietario del grupo.*`, m)
-        if (mentionedJid === ownerBot) return conn.reply(m.chat, `😼 *No puedo eliminar al dueño del bot.*`, m)
-        if (isAdmin) return conn.reply(m.chat, `🍕 *No puedo expulsar a un administrador.*`, m)
+        if (mentionedJid === conn.user.jid) return conn.reply(m.chat, `🎮 *No puedo eliminarme a mí mismo.*`, m)
+        if (mentionedJid === ownerGroup) return conn.reply(m.chat, `🤖 *No puedo expulsar al dueño del lobby.*`, m)
+        if (mentionedJid === ownerBot) return conn.reply(m.chat, `🕹️ *No puedo expulsar al owner del bot.*`, m)
+        if (isAdmin) return conn.reply(m.chat, `🎮 *No puedo expulsar a un admin.*`, m)
 
         await m.react('👢')
         await conn.groupParticipantsUpdate(m.chat, [mentionedJid], 'remove')
 
-        conn.reply(m.chat, `🐱 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗘𝗫𝗣𝗨𝗟𝗦𝗔𝗗𝗢 𓆪 🐱
+        conn.reply(m.chat, `🎮 𓆩 𝗣𝗟𝗔𝗬𝗘𝗥 𝗘𝗫𝗣𝗨𝗟𝗦𝗔𝗗𝗢 𓆪 🤖
 
-.⃟𖥔 ݁. 𖦹˙— \`\`KICK\`\` —˙𖦹.🍕꒷
+.⃟𖥔 ݁. 𖦹˙— \`\`𝗞𝗜𝗖𝗞\`\` —˙𖦹.🕹️꒷
 
-👢 *Usuario:* @${mentionedJid.split('@')[0]}
+👢 *Player:* @${mentionedJid.split('@')[0]}
 👑 *Por:* @${m.sender.split('@')[0]}
 
 ━━━━━━━━━━━
-*Powered by*: ***Garfield Bot Oficial*** 🍕`, m, { mentions: [mentionedJid, m.sender] })
+*Powered by*: ***Ricky Bot Oficial*** 🎮`, m, { mentions: [mentionedJid, m.sender] })
     } catch (e) {
         await m.react('❌')
-        conn.reply(m.chat, `❌ *Se ha producido un problema.*\n> *Error:* ${e.message}`, m)
+        conn.reply(m.chat, `🎮 ❌ *Error del sistema.*\n> *Detalle:* ${e.message} 🤖`, m)
     }
 }
 
