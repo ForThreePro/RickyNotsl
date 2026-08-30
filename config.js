c1dd00bac1df192da8c0c45538c4af1ad9b5713d
YALLICO OWNER <yallico2024@gmail.com>
2026-08-30 14:24:25 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 global.owner = [
-  [ '51927174369', 'Barboza OFC 🌃', true ],
-  [ '573155227977', 'Jota 🐼', true ]
+  [ '15812903813', 'Barboza OFC 🌃', true ],
+  [ '51927174369', 'Jota 🐼', true ]
 ]
 
 global.mods = []
