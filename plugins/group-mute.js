deeb27c12c10a227a7ebace17270ccb6c7216fa4
YALLICO OWNER <yallico2024@gmail.com>
2026-08-30 13:56:12 -0500

    Actualizar group-mute.js

--- plugins/group-mute.js
+++ plugins/group-mute.js
@@ -2,38 +2,38 @@ let mutedUsers = new Set()
 
 let handler = async (m, { conn, command, participants }) => {
     let mentionedJid = m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : false
-    if (!mentionedJid) return m.reply(`🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱
+    if (!mentionedJid) return m.reply(`🎮 𓆩 ***𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🤖
 
 *Uso:*
-.mute @user → Para mutear
+.mute @user → Para mutear del chat
 .unmute @user → Para desmutear
 
-> *Etiqueta a una persona o responde a un mensaje*`)
+> *Etiqueta a un player o responde a su mensaje* 🕹️`)
 
     let isUserAdmin = participants.find(p => p.id === mentionedJid)?.admin
-    if (isUserAdmin) return m.reply(`🍕 *No puedes mutear a un administrador.*`)
-    if (mentionedJid === conn.user.jid) return m.reply(`🍕 *No puedo mutearme a mi mismo.*`)
+    if (isUserAdmin) return m.reply(`🎮 *No puedes mutear a un admin.*`)
+    if (mentionedJid === conn.user.jid) return m.reply(`🤖 *No puedo mutearme a mi mismo.*`)
 
     if (command === "mute") {
-        if (mutedUsers.has(mentionedJid)) return m.reply(`📛 *Este usuario ya está muteado*`)
+        if (mutedUsers.has(mentionedJid)) return m.reply(`📛 *Este player ya está muteado*`)
         mutedUsers.add(mentionedJid)
         await m.react('🔇')
-        conn.reply(m.chat, `🐱 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗠𝗨𝗧𝗘𝗔𝗗𝗢 𓆪 🐱
+        conn.reply(m.chat, `🎮 𓆩 𝗣𝗟𝗔𝗬𝗘𝗥 𝗠𝗨𝗧𝗘𝗔𝗗𝗢 𓆪 🤖
 
-🔇 *Usuario:* @${mentionedJid.split('@')[0]}
+🔇 *Player:* @${mentionedJid.split('@')[0]}
 👑 *Por:* @${m.sender.split('@')[0]}
 
-> *Sus mensajes serán eliminados automaticamente* 🍕`, m, { mentions: [mentionedJid, m.sender] })
+> *Sus mensajes serán eliminados automaticamente* 🕹️`, m, { mentions: [mentionedJid, m.sender] })
     } else if (command === "unmute") {
-        if (!mutedUsers.has(mentionedJid)) return m.reply(`😼 *Este usuario no está muteado*`)
+        if (!mutedUsers.has(mentionedJid)) return m.reply(`🔊 *Este player no está muteado*`)
         mutedUsers.delete(mentionedJid)
         await m.react('🔊')
-        conn.reply(m.chat, `🐱 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗗𝗘𝗦𝗠𝗨𝗧𝗘𝗔𝗗𝗢 𓆪 🐱
+        conn.reply(m.chat, `🎮 𓆩 𝗣𝗟𝗔𝗬𝗘𝗥 𝗗𝗘𝗦𝗠𝗨𝗧𝗘𝗔𝗗𝗢 𓆪 🤖
 
-🔊 *Usuario:* @${mentionedJid.split('@')[0]}
+🔊 *Player:* @${mentionedJid.split('@')[0]}
 👑 *Por:* @${m.sender.split('@')[0]}
 
-> *Ya puede volver a maullar* 🍕`, m, { mentions: [mentionedJid, m.sender] })
+> *Ya puede volver a hablar en el lobby* 🎮`, m, { mentions: [mentionedJid, m.sender] })
     }
 }
 
