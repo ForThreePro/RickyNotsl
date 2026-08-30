b6d404433e73690a235f257777220ce9c0b0420c
YALLICO OWNER <yallico2024@gmail.com>
2026-08-30 13:46:47 -0500

    Actualizar busquedas-googlesearch.js

--- plugins/busquedas-googlesearch.js
+++ plugins/busquedas-googlesearch.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
     let user = `@${m.sender.split('@')[0]}`
     let groupName = m.isGroup? (await conn.groupMetadata(m.chat)).subject : 'Privado'
 
-    if (!text) return m.reply(`🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱\n\n✨ *¿Qué quieres buscar?*\n📌 *Ejemplo:* ${m.prefix}google garfield comiendo lasaña`)
+    if (!text) return m.reply(`🎮 𓆩 ***𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🤖\n\n⚡ *¿Qué quieres buscar?*\n📌 *Ejemplo:* ${m.prefix}google sonic gameplay 2024`)
 
     await m.react('🔍')
 
@@ -14,12 +14,12 @@ let handler = async (m, { conn, text }) => {
 
         if (!results.length) {
             await m.react('❌')
-            return m.reply('🍕 *No encontré resultados.*')
+            return m.reply('🎮 *No encontré resultados. Intenta con otras palabras.* 🤖')
         }
 
-        let txt = `🐱 𓆩 𝗕𝗨𝗦𝗖𝗔𝗗𝗢𝗥 𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𓆪 🐱
+        let txt = `🎮 𓆩 𝗕𝗨𝗦𝗖𝗔𝗗𝗢𝗥 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𓆪 🤖
 
-.⃟𖥔 ݁. 𖦹˙— \`\`RESULTADOS\`\` —˙𖦹.🍕꒷
+.⃟𖥔 ݁. 𖦹˙— \`\`𝗥𝗘𝗦𝗨𝗟𝗧𝗔𝗗𝗢𝗦\`\` —˙𖦹.🕹️꒷
 
 🔎 *Buscando:* ${text}
 
@@ -32,11 +32,11 @@ ${results.map((v, i) => {
         }).join('\n\n')}
 
 👤 *Solicitado por:* ${user}
-🏷 *Grupo:* ${groupName}
+🏷 *Lobby:* ${groupName}
 
 ━━━━━━━━━━━━━━
-*Powered by*: ***Garfield Bot Oficial*** 🍕
-*Tip:* Usa .ytmp4 o .ytmp3 + el link`
+*Powered by*: ***Ricky Bot Oficial*** 🎮
+*Tip:* Usa .ytmp4 o .ytmp3 + el link para descargar`
 
         await conn.reply(m.chat, txt, m, { mentions: [m.sender] })
         await m.react('✅')
@@ -44,7 +44,7 @@ ${results.map((v, i) => {
     } catch (e) {
         console.error(e)
         await m.react('❌')
-        m.reply('🍕 *Error:* No se pudo realizar la búsqueda.')
+        m.reply('🎮 *Error del sistema:* No se pudo realizar la búsqueda. 🤖')
     }
 }
 
