83ac495c7ce2aaf20d483dea05ae51a9892cfb36
YALLICO OWNER <yallico2024@gmail.com>
2026-08-30 13:42:26 -0500

    Actualizar _welcome-audios.js

--- plugins/_welcome-audios.js
+++ plugins/_welcome-audios.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, command, usedPrefix }) => {
 
   // Detectar tipo: welcome / bye / kick
   let type = command.replace('audiowelcome','').replace('audiobye','').replace('audiokick','')
-               .replace('delaudiowelcome','').replace('delaudiobye','').replace('delaudiokick','')
+              .replace('delaudiowelcome','').replace('delaudiobye','').replace('delaudiokick','')
 
   if (command.includes('welcome')) type = 'welcome'
   if (command.includes('bye')) type = 'bye'
@@ -19,25 +19,25 @@ let handler = async (m, { conn, args, command, usedPrefix }) => {
     if (mime && /audio/.test(mime)) {
       let buffer = await q.download()
       chat[`audio${type}`] = buffer
-      return m.reply(`🐱 𓆩 𝗔𝗨𝗗𝗜𝗢 𝗚𝗨𝗔𝗥𝗗𝗔𝗗𝗢 𓆪 🐱\n\n✅ *Audio de ${type} guardado*\nSe reproducirá cuando pase el evento`)
+      return m.reply(`🎮 𓆩 𝗔𝗨𝗗𝗜𝗢 𝗚𝗨𝗔𝗥𝗗𝗔𝗗𝗢 𓆪 🤖\n\n✅ *Audio de ${type} guardado*\nSe reproducirá cuando se active el evento 🕹️`)
     }
 
     // Si manda un link
     if (args[0] && args[0].startsWith('http')) {
       chat[`audio${type}`] = args[0]
-      return m.reply(`🐱 𓆩 𝗟𝗜𝗡𝗞 𝗚𝗨𝗔𝗥𝗗𝗔𝗗𝗢 𓆪 🐱\n\n✅ *Audio de ${type} guardado*\nLink: ${args[0]}`)
+      return m.reply(`🎮 𓆩 𝗟𝗜𝗡𝗞 𝗚𝗨𝗔𝗥𝗗𝗔𝗗𝗢 𓆪 🤖\n\n✅ *Audio de ${type} guardado*\n🔗 Link: ${args[0]}`)
     }
 
-    return m.reply(`🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱\n\n📌 *Uso:* ${usedPrefix}${command} + [responder a audio]\n📌 *Uso:* ${usedPrefix}${command} <link del audio>`)
+    return m.reply(`🎮 𓆩 ***𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🤖\n\n📌 *Uso:* ${usedPrefix}${command} + [responder a audio]\n📌 *Uso:* ${usedPrefix}${command} <link del audio>\n> "Configura tu soundtrack de entrada" 🕹️`)
   }
 
   // DEL AUDIO
   if (command.startsWith('delaudio')) {
     if (!chat[`audio${type}`]) {
-      return m.reply(`🍕 *No hay un audio de ${type} configurado*`)
+      return m.reply(`🎮 *No hay un audio de ${type} configurado* 🤖`)
     }
     delete chat[`audio${type}`]
-    await m.reply(`🐱 𓆩 𝗔𝗨𝗗𝗜𝗢 𝗘𝗟𝗜𝗠𝗜𝗡𝗔𝗗𝗢 𓆪 🐱\n\n❌ *Audio de ${type} eliminado*`)
+    await m.reply(`🎮 𓆩 𝗔𝗨𝗗𝗜𝗢 𝗘𝗟𝗜𝗠𝗜𝗡𝗔𝗗𝗢 𓆪 🤖\n\n❌ *Audio de ${type} eliminado*\n> "Archivo borrado del inventario" 🕹️`)
   }
 }
 
