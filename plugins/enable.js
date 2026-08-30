fd77b430d99cfa010b926706b9cab0f4b3b124b2
YALLICO OWNER <yallico2024@gmail.com>
2026-08-30 13:48:20 -0500

    Actualizar enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -4,53 +4,54 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
   let bot = global.db.data.settings[conn.user.jid] || {}
   let type = command.toLowerCase()
 
-  if (!args[0]) return m.reply(`𓆩 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 𓆪 
+  if (!args[0]) return m.reply(`𓆩 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗖𝗢𝗡𝗙𝗜𝗚 𓆪
 
 ⚙️ *Configuración incorrecta*
 📌 *Uso:* ${usedPrefix + command} on
 📌 *Uso:* ${usedPrefix + command} off
 
-*Ejemplo:* ${usedPrefix + command} on`)
+*Ejemplo:* ${usedPrefix + command} on
+> "Activa o desactiva funciones del sistema" 🕹️`)
 
   let fail = false
   switch (type) {
     case 'welcome': case 'bienvenida':
-      if (m.isGroup &&!isAdmin) { return conn.reply(m.chat, `🌿 *Solo admins*`, m); fail = true; break }
+      if (m.isGroup &&!isAdmin) { return conn.reply(m.chat, `🎮 *Solo admins pueden configurar esto*`, m); fail = true; break }
       chat.bienvenida = isEnable
       break
     case 'subbots': case 'serbot':
-      if (!isROwner) { return conn.reply(m.chat, `💚 *Solo Owner*`, m); fail = true; break }
+      if (!isROwner) { return conn.reply(m.chat, `🤖 *Solo Owner puede activar subbots*`, m); fail = true; break }
       bot.jadibotmd = isEnable
       break
     case 'antispam':
-      if (!isOwner) { return conn.reply(m.chat, `💚 *Solo Owner*`, m); fail = true; break }
+      if (!isOwner) { return conn.reply(m.chat, `🤖 *Solo Owner*`, m); fail = true; break }
       bot.antiSpam = isEnable
       break
     case 'antilink':
-      if (m.isGroup &&!isAdmin) { return conn.reply(m.chat, `🌿 *Solo admins*`, m); fail = true; break }
+      if (m.isGroup &&!isAdmin) { return conn.reply(m.chat, `🎮 *Solo admins*`, m); fail = true; break }
       chat.antiLink = isEnable
       break
     case 'antibot':
-      if (m.isGroup &&!isAdmin) { return conn.reply(m.chat, `🌿 *Solo admins*`, m); fail = true; break }
+      if (m.isGroup &&!isAdmin) { return conn.reply(m.chat, `🎮 *Solo admins*`, m); fail = true; break }
       chat.antiBot = isEnable
       break
     case 'modoadmin':
-      if (m.isGroup &&!isAdmin) { return conn.reply(m.chat, `🌿 *Solo admins*`, m); fail = true; break }
+      if (m.isGroup &&!isAdmin) { return conn.reply(m.chat, `🎮 *Solo admins*`, m); fail = true; break }
       chat.modoadmin = isEnable
       break
     case 'nsfw': case 'antinopor':
-      if (m.isGroup &&!isAdmin) { return conn.reply(m.chat, `🌿 *Solo admins*`, m); fail = true; break }
+      if (m.isGroup &&!isAdmin) { return conn.reply(m.chat, `🎮 *Solo admins*`, m); fail = true; break }
       chat.nsfw = isEnable
       break
     case 'audios':
       chat.audios = isEnable
       break
     case 'autoread': case 'autoleer':
-      if (!isROwner) { return conn.reply(m.chat, `💚 *Solo Owner*`, m); fail = true; break }
+      if (!isROwner) { return conn.reply(m.chat, `🤖 *Solo Owner*`, m); fail = true; break }
       global.opts['autoread'] = isEnable
       break
     case 'antiprivado':
-      if (!isOwner) { return conn.reply(m.chat, `💚 *Solo Owner*`, m); fail = true; break }
+      if (!isOwner) { return conn.reply(m.chat, `🤖 *Solo Owner*`, m); fail = true; break }
       bot.antiPrivate = isEnable
       break
     default:
@@ -59,22 +60,22 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
 
   if (fail) return
 
-  // IMAGEN SAPITO
-  let catalogoImg = { url: 'https://files.evogb.win/QFXQtu.jpg' }
+  // IMAGEN RICKY BOT
+  let catalogoImg = { url: 'https://files.evogb.win/QFXQtu.jpg' } // cambia por tu banner gamer si tienes
 
   let estadoTexto = isEnable? 'Activado 🌀' : 'Desactivado ✖️'
   let estadoEmoji = isEnable? '🟢' : '🔴'
 
-  let statusTxt = ` 𓆩 𝗖𝗢𝗡𝗙𝗜𝗚𝗨𝗥𝗔𝗖𝗜𝗢𝗡 𓆪 
+  let statusTxt = ` 𓆩 𝗖𝗢𝗡𝗙𝗜𝗚𝗨𝗥𝗔𝗖𝗜𝗢𝗡 𓆪
 
-.⃟𖥔 ݁. 𖦹˙— \`\`ON/OFF\`\` —˙𖦹.🍃꒷
+.⃟𖥔 ݁. 𖦹˙— \`\`𝗦𝗜𝗦𝗧𝗘𝗠𝗔 𝗢𝗡/𝗢𝗙\`\` —˙𖦹.🎮꒷
 
 ⚙️ *Función:* ${type}
 📊 *Estado:* ${estadoTexto} ${estadoEmoji}
 👑 *Por:* @${m.sender.split('@')[0]}
 
 ━━━━━━━━━━━
-*Powered by*: GARFIELD BOT PREM 🍃`
+*Powered by*: ***Ricky Bot Oficial*** 🎮`
 
   await conn.sendMessage(m.chat, {
     image: catalogoImg,
