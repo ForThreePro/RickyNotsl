717c47918b770b87e97eb1c9158e937fa7ab2c83
YALLICO OWNER <yallico2024@gmail.com>
2026-09-09 16:37:05 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -21,11 +21,14 @@ global.stickauth = '𝘽𝙮 𝙉𝙤𝙭 𝘽ο𝙩'
 global.wm = '𝙉𝙤𝙭 🌃'
 global.botname = '[ 𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃 ]'
 global.textbot = `𝙋ο𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 𝙉𝙤𝙭 🌀`
-global.dev = '• 𝙋ο𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 𝘾ο𝙢𝙢𝙪𝙣𝙞𝙩𝙮 𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃'
+global.dev = '• 𝙋ο𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 𝘾ο𝙢𝙪𝙣𝙞𝙩𝙮 𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃'
 global.wait = '🌪️ *𝘼𝙜𝙪𝙖𝙧𝙙𝙚 𝙪𝙣 𝙢ο𝙢𝙚𝙣𝙩ο, 𝙨ο𝙮 𝙡𝙚𝙣𝙩ο... ฅ^•ﻌ•^ฅ\n\n> 𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃 🌪️*'
 global.listo = '*𝘼𝙦𝙪𝙞 𝙩𝙞𝙚𝙣𝙚 ฅ^•ﻌ•^ฅ*'
 global.namechannel = '𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃'
 global.channel = 'https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18'
+
+// IMAGEN GLOBAL DEL BOT - Se cambia con .setimg
+global.botimg = 'https://files.evogb.win/OdOIUP.jpg' // imagen por defecto
 
 global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
 
@@ -41,7 +44,7 @@ global.estilo = {
   }, 
   message: { 
     orderMessage: { 
-      itemCount: -999999, 
+      itemCount: -999, 
       status: 1, 
       surface: 1, 
       message: global.packname, 
